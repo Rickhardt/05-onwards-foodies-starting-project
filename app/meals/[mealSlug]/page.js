582b0b6814b945eb428e6f35f8ee3478e9947845
Rickhardt <ricar335@gmail.com>
2024-03-25 22:48:14 -0600

    Commit to use between PCs

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function SharePage() {
+    return(
+        <main>
+            <h1>This page is reserved for dummy pages.</h1>
+        </main>
+    );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function SharePage() {
+    return(
+        <main>
+            <h1>This is the share page for the exercise</h1>
+        </main>
+    );
+}
